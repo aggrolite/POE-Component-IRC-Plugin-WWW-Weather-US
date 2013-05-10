0d9b46ed7b2ff48821060afe38463c3a3493cd09
Curtis Brandt <curtisjbrandt@gmail.com>
2013-05-10 15:02:04 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+POE::Component::IRC::Plugin::WWW::Weather::US - Blah blah blah
+
+# SYNOPSIS
+
+    use POE::Component::IRC::Plugin::WWW::Weather::US;
+
+# DESCRIPTION
+
+POE::Component::IRC::Plugin::WWW::Weather::US is
+
+# AUTHOR
+
+Curtis Brandt <curtis@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2013- Curtis Brandt
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/POE/Component/IRC/Plugin/WWW/Weather/US.pm
@@ -0,0 +1,39 @@
+package POE::Component::IRC::Plugin::WWW::Weather::US;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+POE::Component::IRC::Plugin::WWW::Weather::US - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use POE::Component::IRC::Plugin::WWW::Weather::US;
+
+=head1 DESCRIPTION
+
+POE::Component::IRC::Plugin::WWW::Weather::US is
+
+=head1 AUTHOR
+
+Curtis Brandt E<lt>curtis@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Curtis Brandt
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
